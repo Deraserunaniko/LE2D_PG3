bdf37156ddc6051f3713f462e666f00e7c5a37c8
Deraserunaniko <k024g0076@g.neec.ac.jp>
2026-01-31 23:55:39 +0900

    aaa

--- main.cpp
+++ main.cpp
@@ -3,62 +3,53 @@
 #include <stdlib.h>
 #include <vector>
 #include <list>
+#include <algorithm>
+#include <stdio.h>
+
 
 using namespace std;
 
+int calc(int a, int b) {
+	return a + b;
+}
+
+float calc(float d, float e) {
+	return d + e + 10;
+}
+
+template <typename Type>
+
+Type add(Type a, Type b) {
+	return static_cast<Type>(a + b);
+}
+
+template <typename Type>
+
+Type Min(Type a, Type b) {
+	if (a > b) {
+		return static_cast<Type>(b);
+	} else {
+		return static_cast<Type>(a);
+	}
+}
+
 int main() {
 
+	/*int x = 114;
+	int y = 514;
 
-	 // 山手線の駅名を双方向リストに格納
-	list<const char*> yamanote = {
-		"shinagawa", "osaki", "gotanda", "meguro", "ebisu", "shibuya", "harajuku",
-		"yoyogi", "shinjuku", "shin-okubo", "takadanobaba", "mejiro", "ikebukuro",
-		"otsuka", "sugamo", "komagome", "tabata", "nippori", "uguisudani",
-		"ueno", "okachimachi", "akihabara", "kanda", "tokyo", "yurakucho", "shimbashi",
-		"hamamatsucho", "tamachi"
-	};
+	printf("%d\n", calc(x, y));*/
+
+	/*float l = 3.64f;
+	float m = 3.64f;
+
+	printf("%f\n", calc(l,m));*/
 
 
-	//双方向リスト
-	cout << "1970年" << endl;
-	for (auto itr = yamanote.begin(); itr != yamanote.end(); ++itr) {
-		cout << *itr<<",";
-	}
-
-	cout << endl; // ★ここで改行
-	
-	// 「nippori」の前に「nishi-nippori」を追加
-	for (auto itr = yamanote.begin(); itr != yamanote.end(); ++itr) {
-		if (string(*itr) == "tabata") {
-			yamanote.insert(itr, "nishi-nippori");
-			break; // 1回だけでOKなので抜ける
-		}
-	}
-
-	
-
-	//双方向リスト
-	cout << "2019年" << endl;
-	for (auto itr = yamanote.begin(); itr != yamanote.end(); ++itr) {
-		cout << *itr << ",";
-	}
-	
-	cout << endl; // ★ここで改行
-
-	for (auto itr = yamanote.begin(); itr != yamanote.end(); ++itr) {
-		if (string(*itr) == "tamachi") {
-			yamanote.insert(itr, "takanawa-gateway");
-			break; // 1回だけでOKなので抜ける
-		}
-	}
-
-	//双方向リスト
-	cout << "令和5年" << endl;
-	for (auto itr = yamanote.begin(); itr != yamanote.end(); ++itr) {
-		cout << *itr << ",";
-	}
-
-	cout << endl; // ★ここで改行
+	//計算と結果出力
+	printf("%d\n", Min<int>(128, 256));
+	printf("%f\n", Min<float>(52.5f, 55.0f));
+	printf("%lf\n", Min<double>(3.14159265, 2.7182818281));
 
 	return 0;
 }
